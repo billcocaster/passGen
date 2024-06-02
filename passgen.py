8f758095e3db185208866e07b2590204600a5a7a
billcocaster <116905530+billcocaster@users.noreply.github.com>
2024-06-02 13:39:31 +0300

    Reminders added
    Changes for clean code purposes.
    Reminders added.

--- passgen.py
+++ passgen.py
@@ -12,68 +12,76 @@ class passGen:
     def __init__(self):
         self.main_window = Tk()
         self.main_window.title("Welcome to password generator")
-        self.main_window.geometry('500x500')
+        self.main_window.geometry('650x500')
 
-        self.frame1 = Frame(self.main_window)
-        self.frame2 = Frame(self.main_window)
-        self.checkFrame = Frame(self.main_window)
-        self.frame3 = Frame(self.main_window)
-        self.frame4 = Frame(self.main_window)
+        frame1 = Frame(self.main_window)
+        frame2 = Frame(self.main_window)
+        checkFrame = Frame(self.main_window)
+        frame3 = Frame(self.main_window)
+        frame4 = Frame(self.main_window)
+        self.frame5 = Frame(self.main_window)
 
-        self.brandFont = font.Font(family="helvetica",size=18,slant="roman")
-        self.labelFont = font.Font(family="helvetica",size=18,slant="italic")
+        brandFont = font.Font(family="helvetica",size=18,slant="roman")
+        labelFont = font.Font(family="helvetica",size=18,slant="italic")
+        self.reminderFontLessEq2 = font.Font(family="helvetica",size=10,slant="italic")
+        self.reminderFont3 = font.Font(family="helvetica",size=10,slant="italic") 
         
-        self.brand = Label(self.frame1,text='Please select properties of your password',font=self.brandFont)
-        self.brand.pack(side='top')
+        brand = Label(frame1,text='Please select properties of your password',font=brandFont)
+        brand.pack(side='top')
 
-        self.space = Label(self.frame2,text='',font=('electrolize',15))
-        self.space.pack()
+        space = Label(frame2,text='',font=('electrolize',15))
+        space.pack()
 
-        self.text = Label(self.frame2,text='Length:',font=self.labelFont)
-        self.text.pack(side='left')
+        text = Label(frame2,text='Length:',font=labelFont)
+        text.pack(side='left')
 
-        self.entry = Entry(self.frame2,width=5)
+        self.entry = Entry(frame2,width=5)
         self.entry.pack(side='right')
         
         stil = ttk.Style()
         stil.configure("TCheckbutton", font=('Helvetica', 18, 'italic'))
         
         self.checkSpecVar = tk.IntVar()
-        self.checkSpec = ttk.Checkbutton(self.checkFrame,text='Special chars',style="TCheckbutton",variable=self.checkSpecVar,onvalue=1,offvalue=0)
-        self.checkSpec.pack()        
+        checkSpec = ttk.Checkbutton(checkFrame,text='Special chars',style="TCheckbutton",variable=self.checkSpecVar,onvalue=1,offvalue=0,command=self.reminders)
+        checkSpec.pack()        
         
         self.checkNumVar = tk.IntVar()
-        self.checkNum = ttk.Checkbutton(self.checkFrame,text='Numbers',style="TCheckbutton",variable=self.checkNumVar,onvalue=4,offvalue=0)
-        self.checkNum.pack()        
+        checkNum = ttk.Checkbutton(checkFrame,text='Numbers',style="TCheckbutton",variable=self.checkNumVar,onvalue=4,offvalue=0,command=self.reminders)
+        checkNum.pack()        
         
         self.checkDownVar = tk.IntVar()
-        self.checkDown = ttk.Checkbutton(self.checkFrame,text='Downletters',style="TCheckbutton",variable=self.checkDownVar,onvalue=3,offvalue=0)
-        self.checkDown.pack()        
+        checkDown = ttk.Checkbutton(checkFrame,text='Downletters',style="TCheckbutton",variable=self.checkDownVar,onvalue=3,offvalue=0,command=self.reminders)
+        checkDown.pack()        
 
         self.checkUpVar = tk.IntVar()
-        self.checkUp = ttk.Checkbutton(self.checkFrame,text='Upletters',style="TCheckbutton",variable=self.checkUpVar,onvalue=2,offvalue=0)
-        self.checkUp.pack()        
+        checkUp = ttk.Checkbutton(checkFrame,text='Upletters',style="TCheckbutton",variable=self.checkUpVar,onvalue=2,offvalue=0,command=self.reminders)
+        checkUp.pack()        
 
 
 
-        self.space = Label(self.frame3,text='',font=('electrolize',10))
-        self.space.pack()
+        space = Label(frame3,text='',font=('electrolize',10))
+        space.pack()
         
-        self.button = Button(self.frame3,text='Generate',font='electrolize',command=self.passZone)
-        self.button.pack()
+        button = Button(frame3,text='Generate',font='electrolize',command=self.passZone)
+        button.pack()
         
         self.passGenWord= tk.StringVar()
-        self.entrypass = Entry(self.frame4,textvariable=self.passGenWord,state='readonly')
-        self.entrypass.config(readonlybackground=self.entrypass.cget('background'), fg='black', highlightthickness=0, borderwidth=0)
-        self.entrypass.pack(padx=10,pady=10)
+        entrypass = Entry(frame4,textvariable=self.passGenWord,state='readonly')
+        entrypass.config(readonlybackground=entrypass.cget('background'), fg='black', highlightthickness=0, borderwidth=0)
+        entrypass.pack(padx=10,pady=10)
+        
+        self.reminder = None
+        
+
         
 
 
-        self.frame1.pack()
-        self.frame2.pack()
-        self.checkFrame.pack()
-        self.frame3.pack()
-        self.frame4.pack()
+        frame1.pack()
+        frame2.pack()
+        checkFrame.pack()
+        frame3.pack()
+        frame4.pack()
+        self.frame5.pack()
         
 
         mainloop()
@@ -81,20 +89,21 @@ class passGen:
     def passZone(self):
         try:
             passLength = int(self.entry.get())
+            if passLength<=0:
+                raise ValueError("Length must be a positive number.")
         except ValueError:
             messagebox.showerror("Error","Please enter valid length")
-        selections=[]
-        if self.checkSpecVar.get() !=0:
-            selections.append(self.checkSpecVar.get())
-        if self.checkNumVar.get() !=0:
-            selections.append(self.checkNumVar.get())
-        if self.checkDownVar.get() !=0:
-            selections.append(self.checkDownVar.get())
-        if self.checkUpVar.get() !=0:
-            selections.append(self.checkUpVar.get())
-
+            return
+            
+        selections = self.filters()
+        
+        if not selections:
+            messagebox.showerror("Error", "Please select at least one property")
+            return
+        
         passy=""
-        specialChars=[63,64,33,35,37,43,45,42]
+        specialChars = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 
+                       58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126]
         
         for i in range(passLength):
             selection=random.choice(selections)
@@ -111,6 +120,36 @@ class passGen:
                 o4 = str(random.randint(0,9))
                 passy+=o4
         self.passGenWord.set(passy)
+        
+    def filters(self):
+        selections=[]
+        if self.checkSpecVar.get() !=0:
+            selections.append(self.checkSpecVar.get())
+        if self.checkNumVar.get() !=0:
+            selections.append(self.checkNumVar.get())
+        if self.checkDownVar.get() !=0:
+            selections.append(self.checkDownVar.get())
+        if self.checkUpVar.get() !=0:
+            selections.append(self.checkUpVar.get())
+        return selections
+    
+    def reminders(self):
+        selections = self.filters()
+        if self.reminder:
+            self.reminder.destroy()
+        if len(selections) <= 2:
+            self.reminder = Label(self.frame5, text='Not recommended',foreground='red',font=("helvetica",10,"bold"))
+            self.reminder.pack()
+        elif len(selections) == 3:
+            self.reminder = Label(self.frame5, text="These types of passwords are good but it's recommended to use passwords that contain all these 4 options",foreground='orange',
+                                  font=self.reminderFont3)
+            self.reminder.pack()
+        
+
+            
+        
+        
+        
                 
                 
             
